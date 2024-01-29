20f62a2008e91aa28ddbd6b0a60fba2e966bf351
BuildTools <unconfigured@null.spigotmc.org>
2024-01-29 03:12:04 +0000

    BetterStructures 1.6.7 changelog: - [Fix] Changed the way signs get interpreted by BetterStructures, should fix all signs regardless of WorldEdit version, FastAsyncWorldEdit version and Minecraft version.

--- src/main/java/com/magmaguy/betterstructures/util/WorldEditUtils.java
+++ src/main/java/com/magmaguy/betterstructures/util/WorldEditUtils.java
@@ -1,7 +1,9 @@
 package com.magmaguy.betterstructures.util;
 
 import com.sk89q.jnbt.CompoundTag;
+import com.sk89q.jnbt.StringTag;
 import com.sk89q.worldedit.extent.clipboard.Clipboard;
+import com.sk89q.worldedit.internal.expression.ExpressionException;
 import com.sk89q.worldedit.world.block.BaseBlock;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.Plugin;
@@ -9,7 +11,10 @@ import org.bukkit.util.Vector;
 import org.checkerframework.checker.index.qual.Positive;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Optional;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -34,12 +39,6 @@ public class WorldEditUtils {
             return "";
         }
         CompoundTag data = baseBlock.getNbtData();
-        Optional<Plugin> fawe = Arrays.stream(Bukkit.getPluginManager().getPlugins())
-                .filter(pl -> pl.getName().contains("FastAsyncWorldEdit"))
-                .findFirst();
-        if (fawe.isPresent()) {
-            return getLineFawe(data, line);
-        }
         return getLineWe(data, line);
     }
 
@@ -49,58 +48,22 @@ public class WorldEditUtils {
      * <p>Compatibility with version 1.20.4 has been added.</p>
      */
     private static String getLineWe(@NotNull CompoundTag data, @Positive int line) {
-        List<String> lines = data.toString().lines().toList();
+        try {
+            String text = ((StringTag) data.getValue().get("Text"+line)).getValue();
 
-        for (String s : lines) {
-            s = s.trim().replace("{\"text\":", "").replace("}", ""); // support for versions below 1.20.4
+            Pattern pattern = Pattern.compile("\\{\"text\":\"(.*?)\"\\}");
+            Matcher matcher = pattern.matcher(text);
 
-            if (s.startsWith("TAG_String(\"")) {
-                values.add(s.replace("TAG_String(\"", "").replace("\")", ""));
-                if (values.size() > 3) break;
+            if (matcher.find()) {
+                String extractedText = matcher.group(1);
+                return extractedText;
+            } else {
+                throw new Exception();
             }
-        }
-        // lines in versions below 1.20.4 duplicated twice - note this is just for versions prior to 1.20.4
-        if (data.toString().contains("{\"text\":")) {
-            Matcher matcher = Pattern.compile("Text[1-4]=LinStringTag\\[\\{\"text\":\"(.*?)\"").matcher(data.toString());
-            while (matcher.find()) {
-                values.add(matcher.group(1));
-            }
-            Collections.reverse(values);
+        } catch (Exception ex) {
+            Bukkit.getLogger().warning("Unexpected sign format!" + data);
         }
 
-        if (line <= values.size()) {
-            return values.get(line - 1);
-        }
-        return "";
-    }
-
-    /**
-     * <p>Parses data from a sign's NBT and returns the specified line number.
-     * Designed for <b>FastAsyncWorldEdit</b> NBT format.</p>
-     * <p>Compatibility with version 1.20.4 has been added.</p>
-     */
-    private static String getLineFawe(@NotNull CompoundTag data, @Positive int line) {
-        // 1.20.4 have a different format of NBT
-        String str = data.toString()
-                .replace("{\\\\\"text\\\\\":", "")
-                .replace("}", "");
-
-        Matcher matcher = Pattern.compile("value=\"\\\\\\\\\"(.*?)\\\\\\\\\"\"")
-                .matcher(str);
-
-        while (values.size() < 5 && matcher.find()) {
-            String match = matcher.group(1);
-            if (match.isEmpty()) continue;
-            values.add(match);
-        }
-        // versions below 1.20.4 have reversed order of the lines (from 4 to 1)
-        if (data.toString().contains("{\\\\\"text\\\\\":")) {
-            Collections.reverse(values);
-        }
-
-        if (line <= values.size()) {
-            return values.get(line - 1);
-        }
         return "";
     }
 }
