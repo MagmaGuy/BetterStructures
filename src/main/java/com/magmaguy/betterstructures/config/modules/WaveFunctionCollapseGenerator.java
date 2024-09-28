e953fdb3326e9e01d8b39422c75115c682c68528
BuildTools <unconfigured@null.spigotmc.org>
2024-09-28 17:06:59 +0100

    - now fully working, with faster generation and a showcase more plus an instant mode

--- src/main/java/com/magmaguy/betterstructures/config/modules/WaveFunctionCollapseGenerator.java
+++ src/main/java/com/magmaguy/betterstructures/config/modules/WaveFunctionCollapseGenerator.java
@@ -6,10 +6,7 @@ import com.magmaguy.betterstructures.modules.ChunkData;
 import com.magmaguy.betterstructures.modules.ModulesContainer;
 import com.magmaguy.magmacore.util.Logger;
 import lombok.Getter;
-import org.bukkit.GameMode;
-import org.bukkit.Location;
-import org.bukkit.World;
-import org.bukkit.WorldCreator;
+import org.bukkit.*;
 import org.bukkit.entity.Player;
 import org.bukkit.generator.ChunkGenerator;
 import org.bukkit.scheduler.BukkitRunnable;
@@ -22,12 +19,17 @@ import java.util.concurrent.ThreadLocalRandom;
 public class WaveFunctionCollapseGenerator {
     @Getter
     private static final List<Integer> validRotations = Arrays.asList(0, 90, 180, 270);
-    private final HashSet<Vector3i> emptyChunks = new HashSet<>();
+    private final HashSet<ChunkData> emptyChunks = new HashSet<>();
     private final Player player;
     private final Map<Vector3i, ChunkData> chunkMap = new HashMap<>();
     private final int interval;
     private final int radius;
+    private final boolean systemShowcaseSpeed = false;
+    private final int massPasteSize = 20;
     private World world;
+    private int processedChunks = 0;
+    private int massPasteCount = 0;
+    private long startTime;
 
     public WaveFunctionCollapseGenerator(String worldName, int radius, int interval, Player player, String startingModule) {
         this.player = player;
@@ -35,7 +37,12 @@ public class WaveFunctionCollapseGenerator {
         this.radius = radius;
         generateWorld(worldName);
         initializeChunkData();
-        start(startingModule);
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                start(startingModule);
+            }
+        }.runTaskAsynchronously(MetadataHandler.PLUGIN);
     }
 
     private void initializeChunkData() {
@@ -59,10 +66,10 @@ public class WaveFunctionCollapseGenerator {
 
     @Nullable
     private Vector3i getClosestEmptyChunkLocationKey(List<Vector3i> elements) {
-        Logger.debug("empty size: " + elements.size());
         Vector3i selectedLocation = null;
-        if (elements.isEmpty()) {return selectedLocation;}
-
+        if (elements.isEmpty()) {
+            return selectedLocation;
+        }
         if (elements.size() > 1) {
             double smallestDistance = Double.MAX_VALUE;
             for (Vector3i chunkLocation : elements) {
@@ -72,15 +79,10 @@ public class WaveFunctionCollapseGenerator {
                     selectedLocation = chunkLocation;
                 }
             }
-            Logger.debug("placing at " + selectedLocation);
-
             return selectedLocation;
         } else {
             selectedLocation = elements.get(0);
         }
-
-        Logger.debug("placing at " + selectedLocation);
-
         return selectedLocation;
     }
 
@@ -91,11 +93,12 @@ public class WaveFunctionCollapseGenerator {
         worldCreator.generator(new VoidGenerator());
         world = worldCreator.createWorld();
         world.setAutoSave(false);
-        player.teleport(new Location(world, -8, 16, -8));
+        player.teleport(new Location(world, 8, 16, 8));
         player.setGameMode(GameMode.SPECTATOR);
     }
 
     private void start(String startingModule) {
+        startTime = System.currentTimeMillis();
         ModulesContainer modulesContainer = ModulesContainer.getModulesContainers().get(startingModule);
         paste(new Vector3i(), modulesContainer, validRotations.get(ThreadLocalRandom.current().nextInt(0, validRotations.size()))); //todo reenable rotations
         // Begin the recursive generation
@@ -106,9 +109,13 @@ public class WaveFunctionCollapseGenerator {
         int mostElements = 0;
         List<Vector3i> elements = new ArrayList<>();
 
-        for (ChunkData chunkData : chunkMap.values()) {
+        Iterator<ChunkData> iterator = emptyChunks.iterator();
+
+        while (iterator.hasNext()) {
+            ChunkData chunkData = iterator.next();
 
             if (chunkData.isGenerated() || chunkData.canOnlyBeNothing()) {
+                iterator.remove();
                 continue;
             }
 
@@ -123,25 +130,48 @@ public class WaveFunctionCollapseGenerator {
             }
         }
 
-        Logger.debug("most elements for border: " + mostElements);
         Vector3i selectedChunkLocationKey = getClosestEmptyChunkLocationKey(elements);
+        if (selectedChunkLocationKey == null) {
+            done();
+            return;
+        }
         generateNextChunk(selectedChunkLocationKey);
 
-        if (selectedChunkLocationKey != null) {
+        if (systemShowcaseSpeed) {
             new BukkitRunnable() {
                 @Override
                 public void run() {
                     searchNextChunkToGenerate();
                 }
             }.runTaskLater(MetadataHandler.PLUGIN, interval);
-        } else player.sendTitle("Done!", "Generation complete!");
+        } else if (processedChunks % 1000 != 0) {
+            searchNextChunkToGenerate();
+        } else {
+            // Avoid stack overflows
+            new BukkitRunnable() {
+                @Override
+                public void run() {
+                    searchNextChunkToGenerate();
+                }
+            }.runTaskLaterAsynchronously(MetadataHandler.PLUGIN, 1);
+        }
     }
 
+
     private void paste(Vector3i chunkLocation, ModulesContainer modulesContainer, Integer rotation) {
-        Location pasteLocation = new Location(world, chunkLocation.x * 16, chunkLocation.y * 16, chunkLocation.z * 16);
-        if (modulesContainer != null && modulesContainer.getClipboard() != null)
-            Module.paste(modulesContainer.getClipboard(), pasteLocation.clone().add(-17, 0, -17), rotation);
-        chunkMap.get(chunkLocation).paste(new ModulesContainer.PastableModulesContainer(modulesContainer, rotation));
+        if (processedChunks % 500 == 0) {
+            Logger.debug("Processed " + processedChunks + " chunks, latest location " + chunkLocation.toString() + " with rotation " + rotation);
+        }
+        processedChunks++;
+        if (systemShowcaseSpeed) actualPaste(chunkMap.get(chunkLocation));
+        chunkMap.get(chunkLocation).processPaste(new ModulesContainer.PastableModulesContainer(modulesContainer, rotation));
+    }
+
+    private void actualPaste(ChunkData chunkData) {
+        ModulesContainer modulesContainer = chunkData.getModulesContainer();
+        if (modulesContainer == null || modulesContainer.getClipboard() == null) return;
+        Location pasteLocation = new Location(world, chunkData.getChunkLocation().x * 16, chunkData.getChunkLocation().y * 16, chunkData.getChunkLocation().z * 16);
+        Module.paste(modulesContainer.getClipboard(), pasteLocation.clone().add(-1, 0, -1), chunkData.getRotation());
     }
 
     private void generateNextChunk(Vector3i nextChunkKey) {
@@ -163,6 +193,80 @@ public class WaveFunctionCollapseGenerator {
         player.sendMessage("Rolling back invalid chunk at (" + location.x + ", " + location.y + ", " + location.z + ")");
         chunkData.hardReset();
     }
+
+    private void done() {
+        if (player != null) {
+            player.sendTitle("Done!", "Generation complete!");
+        }
+        Logger.warn("Done with infinity generator!");
+        if (!systemShowcaseSpeed) {
+            Logger.warn("Starting mass paste");
+            if (player != null) {
+                player.sendMessage("Starting mass paste...");
+            }
+            Bukkit.getScheduler().runTask(MetadataHandler.PLUGIN, () -> instantPaste(-radius, -radius));
+        } else {
+            // Calculate and display the elapsed time
+            long endTime = System.currentTimeMillis();
+            long elapsedTime = endTime - startTime;
+
+            long seconds = (elapsedTime / 1000) % 60;
+            long minutes = (elapsedTime / (1000 * 60)) % 60;
+            long hours = elapsedTime / (1000 * 60 * 60);
+
+            String timeString = String.format("%02d:%02d:%02d", hours, minutes, seconds);
+
+            if (player != null) {
+                player.sendMessage("Generation complete! Time taken: " + timeString);
+            }
+            Logger.warn("Generation complete! Time taken: " + timeString);
+        }
+    }
+
+
+    private void instantPaste(int currentX, int currentZ) {
+        massPasteCount++;
+        if (massPasteCount % massPasteSize == 0) {
+            Logger.debug("Pasting chunk " + currentX + ", " + currentZ);
+        }
+        int x = currentX + 1;
+        int z = currentZ;
+        if (x > radius) {
+            x = -radius;
+            z++;
+        }
+        if (z > radius) {
+            // Mass paste is complete
+            if (player != null) {
+                player.sendMessage("Mass paste complete!");
+
+                // Calculate and display the elapsed time
+                long endTime = System.currentTimeMillis();
+                long elapsedTime = endTime - startTime;
+
+                long seconds = (elapsedTime / 1000) % 60;
+                long minutes = (elapsedTime / (1000 * 60)) % 60;
+                long hours = elapsedTime / (1000 * 60 * 60);
+
+                String timeString = String.format("%02d:%02d:%02d", hours, minutes, seconds);
+
+                player.sendMessage("Total time taken (including mass paste): " + timeString);
+                Logger.warn("Total time taken (including mass paste): " + timeString);
+            }
+            return;
+        }
+        for (int y = -4; y <= 20; y++) {
+            actualPaste(chunkMap.get(new Vector3i(x, y, z)));
+        }
+        int finalX = x;
+        int finalZ = z;
+        if (massPasteCount % massPasteSize == 0) {
+            Bukkit.getScheduler().runTaskLater(MetadataHandler.PLUGIN, () -> instantPaste(finalX, finalZ), 1);
+        } else {
+            instantPaste(finalX, finalZ);
+        }
+    }
+
 
     private static class VoidGenerator extends ChunkGenerator {
     }
