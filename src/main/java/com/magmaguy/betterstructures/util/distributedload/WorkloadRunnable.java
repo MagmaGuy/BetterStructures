dd7cb9f517b4f269cca3bacc7c8caf5f42f3ef29
MagmaGuy <tiagoarnaut@gmail.com>
2025-02-15 21:37:53 +0000

    - now using workloads to place blocks without affecting tps

---
+++ src/main/java/com/magmaguy/betterstructures/util/distributedload/WorkloadRunnable.java
@@ -0,0 +1,41 @@
+package com.magmaguy.betterstructures.util.distributedload;
+
+import com.magmaguy.magmacore.util.Logger;
+import org.bukkit.scheduler.BukkitRunnable;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class WorkloadRunnable extends BukkitRunnable {
+    private final Deque<Workload> workloadDeque = new ArrayDeque<>();
+    private final long maxMillisPerTick;
+    private final Runnable onComplete;
+
+    public WorkloadRunnable(double percentageOfTick, Runnable onComplete) {
+        this.maxMillisPerTick = Math.max((long) ((1.0 / 20.0) * percentageOfTick * 1000), 2L);
+        this.onComplete = onComplete;
+    }
+
+    public void addWorkload(Workload workload) {
+        workloadDeque.add(workload);
+    }
+
+    @Override
+    public void run() {
+        long stopTime = System.currentTimeMillis() + maxMillisPerTick;
+        while (!workloadDeque.isEmpty() && System.currentTimeMillis() < stopTime) {
+            Workload workload = workloadDeque.poll();
+            if (workload != null) {
+                workload.compute();
+            }
+        }
+
+        if (workloadDeque.isEmpty()) {
+            if (onComplete != null) {
+                onComplete.run();
+            }
+            this.cancel();
+            Logger.debug("done");
+        }
+    }
+}
